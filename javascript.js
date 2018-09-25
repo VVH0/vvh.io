787593c0c1f95b49d77df3e0b8d597ec403a8d45
VALEUR Virginie <valeurvirginie@MacBook-Pro-de-VALEUR.local>
2018-09-25 20:32:44 +0200

    maj javascrpt

---
+++ javascript.js
@@ -0,0 +1,15 @@
+function mypopup()
+
+{
+var x = confirm("hey toi!!Viens sur mon site");
+
+                if(x === true)
+
+                  {
+
+                  document.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+
+                  }
+
+ }
+            setTimeout (mypopup, 3000);
